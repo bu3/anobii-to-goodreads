cbb5306e305a8ee387d4e4d8c24f7a1f6b63c97a
Fabio Mangione <fmangione@salesforce.com>
2025-01-08 00:54:13 +0000

    acceptance test pass

--- acceptance/acceptance_test.go
+++ acceptance/acceptance_test.go
@@ -1,17 +1,19 @@
 package acceptance_test
 
 import (
+	"bufio"
 	"github.com/bu3/anobii-to-goodreads/convert"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"log"
 	"os"
+	"strings"
 )
 
 var _ = Describe("Convert", func() {
 	Context("Anobii to Goodreads", func() {
-		XIt("should match expected file", func() {
-			expectedContent, err := os.ReadFile("../testdata/goodreads_expected.csv")
+		It("should match expected file", func() {
+			expectedContent, err := readAndNormalize("../testdata/goodreads_expected.csv")
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -20,7 +22,7 @@ var _ = Describe("Convert", func() {
 			err = convert.Convert("../testdata/anobii-exported.csv", generatedFile)
 			Expect(err).ToNot(HaveOccurred())
 
-			generatedFileContent, err := os.ReadFile(generatedFile)
+			generatedFileContent, err := readAndNormalize(generatedFile)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -30,3 +32,24 @@ var _ = Describe("Convert", func() {
 	})
 
 })
+
+func readAndNormalize(filename string) ([]byte, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var builder strings.Builder
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		builder.WriteString(scanner.Text())
+		builder.WriteString("\n") // Normalize line endings to '\n'
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return []byte(builder.String()), nil
+}
