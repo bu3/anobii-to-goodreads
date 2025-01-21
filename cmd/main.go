41d0291e8fa7ae5d421aae7d7e830c26cb0609ee
Fabio Mangione <fmangione@salesforce.com>
2025-01-21 22:53:33 +0000

    fix test and add main

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"flag"
+	"github.com/bu3/anobii-to-goodreads/pkg/convert"
+)
+
+var (
+	inputFile  string
+	outputFile string
+)
+
 func main() {
+	flag.StringVar(&inputFile, "in", "", "path to the input file")
+	flag.StringVar(&outputFile, "out", "", "path to the converted file")
+	flag.Parse()
+
+	if inputFile == "" || outputFile == "" {
+		panic("input and output file must be specified")
+	}
+
+	err := convert.Convert(inputFile, outputFile)
+	if err != nil {
+		panic("Error converting input file to output file: " + err.Error())
+	}
 }
